a904dae79cab1d9bc7cf2d8cd246d6fbdc746fed
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2024-02-25 23:45:28 +0000

    Play a random move

--- SharpStep.Console/Executor.fs
+++ SharpStep.Console/Executor.fs
@@ -2,8 +2,36 @@ namespace SharpStep.Console
 
 open SharpStep.Core
 open System.Reactive.Linq
+open System.Threading.Tasks
+open System
 
 module Executor =
+
+    let rand = Random()
+
+    let private indices (Board board) : (int * int) list =
+        board
+        |> List.mapi (fun i row ->
+            row
+            |> List.mapi (fun j cell ->
+                match cell with
+                | Playable -> Some(i, j)
+                | _ -> None))
+        |> List.collect id
+        |> List.choose id
+
+    let private move (board: Board) : Task<Response option> =
+        task {
+            let playable = indices board
+
+            if List.isEmpty playable then
+                return None
+            else
+                let index = rand.Next(playable.Length)
+                let (i, j) = playable.[index]
+                return (j, i) |> BestMove |> Some
+        }
+
     let execute (command: Command) : StdOut =
         match command with
         | Handshake version ->
@@ -16,5 +44,10 @@ module Executor =
             |> Identification
             |> Response.strings
             |> Observable.ToObservable
-        | Move _ -> Observable.Empty()
+        | Move ((board, _), _) ->
+            Observable.StartAsync(fun () -> move board)
+            |> Observable.choose id
+            |> Observable.map Response.strings
+            |> Observable.map Observable.ToObservable
+            |> Observable.Concat
         | Quit -> Observable.Empty()
